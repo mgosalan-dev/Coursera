bd1b9d350abdb04ee8bc390579f8a755dcb524eb
Manoel Gosalan <mgosalan.dev@gmail.com>
2025-08-20 21:42:52 +0100

    change folder name

--- Exemplos/js/script3.js
+++ Exemplos/js/script3.js
@@ -4,5 +4,3 @@ var idade = Number(prompt("Digite sua idade: "));
    //console.log("Olá, " + nome + "!");
    // as crases são sensíveis, por isso o nome da variável deve ser exatamente igual e ele é a forma mais limpa de concatenar strings
 alert(`Olá, ${nome}! Sua idade é ${idade} anos.`);
-
-
---
+++ Exemplos/js/script5.js
@@ -0,0 +1,29 @@
+// Operadores aritméticos de atribuição e subritração
+let a = 10;
+let b = 5;
+
+// Soma
+const soma = a + b; // 15
+
+// Subtração
+const subtracao = a - b; // 5
+
+// Multiplicação
+const multiplicacao = a * b; // 50
+
+// Divisão
+const divisao = a / b; // 2
+
+// Módulo (resto da divisão)
+const modulo = a % b; // 0
+
+// Exponenciação
+const exponenciacao = a ** b; // 100000
+
+// Incremento
+let incremento = a + 1; // 11
+let incrementoPre = a + 2; // 12
+
+// Decremento
+let decremento = a - 1; // 9
+let decrementoPre = a - 2; // 8
